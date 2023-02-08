edf4e9eada6dda4f743ad01da48e8cd05c082693
edgar-philipp <edgar.philipp@gmail.com>
2023-02-08 16:10:12 +0100

    Update transform_to_github_matrix.sh

--- transform_to_github_matrix.sh
+++ transform_to_github_matrix.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+echo "::group::transform-to-github-matrix"
+
+changed_files="$CHANGED_FILES"
+
+for i in "${!changed_files[@]}"; do
+  changed_files[$i]="{\"file\": \"${changed_files[$i]}\"}"
+done
+
+matrix=$(echo "{}")
+
+if [ -n "$changed_files" ]; then
+  json_elements=$(IFS=, ; echo "${changed_files[*]}")
+  matrix=$(echo "{\"include\":[$json_elements]}")
+fi
+
+echo "Matrix: $matrix"
+echo "matrix=$matrix" >> $GITHUB_OUTPUT
+
+echo "::endgroup::"
