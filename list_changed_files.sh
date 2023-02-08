79f02ec44ee7a2ae9d886a71379fb924dbd62959
edgar-philipp <edgar.philipp@gmail.com>
2023-02-08 15:40:03 +0100

    Update list_changed_files.sh

--- list_changed_files.sh
+++ list_changed_files.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Goodbye"
+echo "::group::list-changed-files"
+
+list=$(git diff-tree --diff-filter=M --no-commit-id --name-only -r $CURRENT_HASH -- 'openapi/**/*.yml' 'openapi/**/*.yaml')
+echo "Changed files: $list"
+
+changed_files="(${list[*]//$'\n'/ })"
+echo "changed_files=${changed_files}" >> $GITHUB_OUTPUT
+
+echo "::endgroup::"
