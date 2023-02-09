25fdc3cd71e911f98b84148619d6532679493186
edgar-philipp <edgar.philipp@gmail.com>
2023-02-09 11:07:17 +0100

    Update list_changed_files.sh

--- list_changed_files.sh
+++ list_changed_files.sh
@@ -8,7 +8,7 @@ echo $(git log --oneline)
 # filter M = modified
 command="git diff-tree --diff-filter=M --no-commit-id --name-only -r $CURRENT_HASH -- '${PATH_FILTER}/*.yml' '${PATH_FILTER}/*.yaml'"
 echo $command 
-list=$( echo $command)
+list=$( eval $command)
 echo "Changed files: $list"
 
 changed_files="${list[*]//$'\n'/ }"
