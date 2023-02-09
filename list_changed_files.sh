1aa1335d7312d81a6d86ff59ff0180df6dd6e410
edgar-philipp <edgar.philipp@gmail.com>
2023-02-09 10:16:29 +0100

    Update list_changed_files.sh

--- list_changed_files.sh
+++ list_changed_files.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "::group::list-changed-files"
+set -x #echo on
 
-echo "git log --oneline"
 echo $(git log --oneline)
 
 # filter M = modified
-echo "git diff-tree --diff-filter=M --no-commit-id --name-only -r $CURRENT_HASH -- '${PATH_FILTER}/*.yml' '${PATH_FILTER}/*.yaml'"
 list=$(git diff-tree --diff-filter=M --no-commit-id --name-only -r $CURRENT_HASH -- '${PATH_FILTER}/*.yml' '${PATH_FILTER}/*.yaml')
 echo "Changed files: $list"
 
 changed_files="${list[*]//$'\n'/ }"
 echo "changed_files=${changed_files}" >> $GITHUB_OUTPUT
 
+set +x
 echo "::endgroup::"
